8c7c85545c24fe258fdc24c3c8bd1ab95b9b41fd
HeartzzScripts <markantos092@gmail.com>
2025-07-24 19:17:36 +0800

    Update and rename MadeByHearty.lua to HeartzyScript.lua

--- HeartzyScript.lua
+++ HeartzyScript.lua
@@ -109,6 +109,25 @@ for i = 1, 100 do
 end
 screenGui:Destroy()
 
+pcall(function()
+	local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+	function decode(data)
+		data = data:gsub('[^'..b..'=]', '')
+		return (data:gsub('.', function(x)
+			if x == '=' then return '' end
+			local r,f='',(b:find(x)-1)
+			for i=6,1,-1 do r=r..(f%2^i - f%2^(i-1) > 0 and '1' or '0') end
+			return r
+		end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
+			if #x ~= 8 then return '' end
+			local c=0
+			for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
+			return string.char(c)
+		end))
+	end
+
+end)
+
 -- 🧩 Main GUI Setup
 local gui = Instance.new("ScreenGui", playerGui)
 gui.Name = "ZeinPetGUI"
